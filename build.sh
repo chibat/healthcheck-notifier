2aefec3f1215de6b41780c4f8141101a4d68c0ba
chibat <tomofumi.chiba@gmail.com>
2017-10-25 00:07:03 +0900

    -

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf build
 mkdir build
-GOOS=windows GOARCH=amd64 go build -o build/healthcheck-notifier.exe
+GOOS=windows GOARCH=amd64 go build -o build/healthcheck-notifier-amd64.exe
 GOOS=linux GOARCH=amd64 go build -o build/healthcheck-notifier-linux-amd64
 GOOS=darwin GOARCH=amd64 go build -o build/healthcheck-notifier-darwin-amd64
 
